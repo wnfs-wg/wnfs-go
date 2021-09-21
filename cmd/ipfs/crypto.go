1822efbf91d77fb710980326ebc630774b5d38f7
Brendan O'Brien <sparkle_pony_2000@qri.io>
2021-09-21 11:18:13 -0400

    refactor(cipherchunker): remove Adder from cipherchunker

--- cmd/ipfs/crypto.go
+++ cmd/ipfs/crypto.go
@@ -7,12 +7,14 @@ import (
 	"io"
 	"io/fs"
 
-	blockservice "github.com/ipfs/go-blockservice"
 	cid "github.com/ipfs/go-cid"
-	"github.com/ipfs/go-filestore"
-	blockstore "github.com/ipfs/go-ipfs-blockstore"
+	cidutil "github.com/ipfs/go-cidutil"
 	ipld "github.com/ipfs/go-ipld-format"
 	dag "github.com/ipfs/go-merkledag"
+	merkledag "github.com/ipfs/go-merkledag"
+	balanced "github.com/ipfs/go-unixfs/importer/balanced"
+	ihelper "github.com/ipfs/go-unixfs/importer/helpers"
+	"github.com/multiformats/go-multihash"
 	cipherchunker "github.com/qri-io/wnfs-go/cipherchunker"
 	cipherfile "github.com/qri-io/wnfs-go/cipherfile"
 	mdstore "github.com/qri-io/wnfs-go/mdstore"
@@ -52,79 +54,99 @@ func (fs *Filestore) PutEncryptedFile(f fs.File, key []byte) (mdstore.PutResult,
 		return mdstore.PutResult{}, err
 	}
 
-	id, err := fs.putEncryptedFile(f, auth)
+	node, err := fs.putEncryptedFile(f, auth)
 	if err != nil {
 		return mdstore.PutResult{}, err
 	}
 
-	// storedFile, err := fs.capi.Unixfs().Get(fs.ctx, corepath.IpfsPath(id))
-	// if err != nil {
-	// 	return mdstore.PutResult{}, err
-	// }
-
-	// size, err := storedFile.Size()
-	// if err != nil {
-	// 	return mdstore.PutResult{}, err
-	// }
-
 	return mdstore.PutResult{
-		Cid:  id,
+		Cid:  node.Cid(),
 		Size: 1,
 	}, nil
 }
 
-func (fs *Filestore) putEncryptedFile(f fs.File, auth cipher.AEAD) (cid.Cid, error) {
-	addblockstore := fs.node.Blockstore
-	exch := fs.node.Exchange
-	pinning := fs.node.Pinning
-	bserv := blockservice.New(addblockstore, exch) // hash security 001
-	dserv := dag.NewDAGService(bserv)
+func (fs *Filestore) putEncryptedFile(f fs.File, auth cipher.AEAD) (ipld.Node, error) {
+	prefix, err := merkledag.PrefixForCidVersion(1)
+	if err != nil {
+		return nil, err
+	}
+	prefix.MhType = multihash.SHA2_256
 
-	// add a sync call to the DagService
-	// this ensures that data written to the DagService is persisted to the underlying datastore
-	// TODO: propagate the Sync function from the datastore through the blockstore, blockservice and dagservice
-	syncDserv := &syncDagService{
-		DAGService: dserv,
-		syncFn: func() error {
-			ds := fs.node.Repo.Datastore()
-			if err := ds.Sync(blockstore.BlockPrefix); err != nil {
-				return err
-			}
-			return ds.Sync(filestore.FilestorePrefix)
+	spl, err := cipherchunker.NewCipherSplitter(f, auth, 1024*256)
+	if err != nil {
+		return nil, err
+	}
+
+	dbp := ihelper.DagBuilderParams{
+		Maxlinks:  1024,
+		RawLeaves: true,
+
+		CidBuilder: cidutil.InlineBuilder{
+			Builder: prefix,
+			Limit:   32,
 		},
+
+		Dagserv: fs.node.DAG,
 	}
 
-	fileAdder, err := cipherchunker.NewAdder(fs.ctx, addblockstore, pinning, syncDserv, auth)
+	db, err := dbp.New(spl)
 	if err != nil {
-		return cid.Undef, err
+		return nil, err
 	}
 
-	// fileAdder.Chunker = settings.Chunker
-	// if settings.Events != nil {
-	// 	fileAdder.Out = settings.Events
-	// 	fileAdder.Progress = settings.Progress
+	return balanced.Layout(db)
+
+	// addblockstore := fs.node.Blockstore
+	// exch := fs.node.Exchange
+	// pinning := fs.node.Pinning
+	// bserv := blockservice.New(addblockstore, exch) // hash security 001
+	// dserv := dag.NewDAGService(bserv)
+
+	// // add a sync call to the DagService
+	// // this ensures that data written to the DagService is persisted to the underlying datastore
+	// // TODO: propagate the Sync function from the datastore through the blockstore, blockservice and dagservice
+	// syncDserv := &syncDagService{
+	// 	DAGService: dserv,
+	// 	syncFn: func() error {
+	// 		ds := fs.node.Repo.Datastore()
+	// 		if err := ds.Sync(blockstore.BlockPrefix); err != nil {
+	// 			return err
+	// 		}
+	// 		return ds.Sync(filestore.FilestorePrefix)
+	// 	},
 	// }
-	// fileAdder.Pin = settings.Pin && !settings.OnlyHash
-	// fileAdder.Silent = settings.Silent
-	// fileAdder.RawLeaves = settings.RawLeaves
-	// fileAdder.NoCopy = settings.NoCopy
-	// fileAdder.CidBuilder = prefix
 
-	// switch settings.Layout {
-	// case options.BalancedLayout:
-	// 	// Default
-	// case options.TrickleLayout:
-	// 	fileAdder.Trickle = true
-	// default:
-	// 	return nil, fmt.Errorf("unknown layout: %d", settings.Layout)
+	// fileAdder, err := cipherchunker.NewAdder(fs.ctx, addblockstore, pinning, syncDserv, auth)
+	// if err != nil {
+	// 	return cid.Undef, err
 	// }
 
-	nd, err := fileAdder.AddFileAndPin(f)
-	if err != nil {
-		return cid.Undef, err
-	}
+	// // fileAdder.Chunker = settings.Chunker
+	// // if settings.Events != nil {
+	// // 	fileAdder.Out = settings.Events
+	// // 	fileAdder.Progress = settings.Progress
+	// // }
+	// // fileAdder.Pin = settings.Pin && !settings.OnlyHash
+	// // fileAdder.Silent = settings.Silent
+	// // fileAdder.RawLeaves = settings.RawLeaves
+	// // fileAdder.NoCopy = settings.NoCopy
+	// // fileAdder.CidBuilder = prefix
 
-	return nd.Cid(), nil
+	// // switch settings.Layout {
+	// // case options.BalancedLayout:
+	// // 	// Default
+	// // case options.TrickleLayout:
+	// // 	fileAdder.Trickle = true
+	// // default:
+	// // 	return nil, fmt.Errorf("unknown layout: %d", settings.Layout)
+	// // }
+
+	// nd, err := fileAdder.AddFileAndPin(f)
+	// if err != nil {
+	// 	return cid.Undef, err
+	// }
+
+	// return nd.Cid(), nil
 }
 
 // syncDagService is used by the Adder to ensure blocks get persisted to the underlying datastore
